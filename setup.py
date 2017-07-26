fe81ef976fd1b66b1c7baff530578712cab8d354
Marinus <marinuso@gmail.com>
2017-07-26 15:17:10 +0100

    restructure to make it a Python package
    Former-commit-id: 4499aff225540c16fc823a7cf3de205e5e7e2860

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+setup(
+        name='APLPy',
+        version='0.0.1.dev1',
+        
+        description='Python - Dyalog APL bridge',
+        long_description="""
+This package allows communication between Python and Dyalog APL.
+""",
+        
+        url='TODO',
+        
+        author='TODO',
+        author_email='TODO',
+
+        license='TODO',
+
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+
+            'Intended Audience :: Developers',
+
+            'Programming Language :: APL',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+        ],
+
+        keywords='apl dyalog-apl bridge',
+
+        packages=['aplpy'],
+
+        package_data={
+            'aplpy': [
+                'Py.dyalog',
+                'PyTest.dyalog',
+                'WinPySlave.dyalog',
+                'WinPySlave.dyapp'
+            ]
+        },
+)
