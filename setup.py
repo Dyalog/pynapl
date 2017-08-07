d19e0e67ae7cb639719d114dcea2ece554e78f94
Marinus <marinuso@gmail.com>
2017-08-07 15:13:06 +0100

    bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='APLPy',
-        version='0.0.3.dev3',
+        version='0.0.3.dev4',
         
         description='Python - Dyalog APL bridge',
         long_description="""
